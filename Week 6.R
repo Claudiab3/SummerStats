ed84e51c73c2b898394b096da57b1cfca6051675
Claudiab3 <cbobadi5@gmail.com>
2023-08-02 22:26:20 +0000

    IN-CLASS

---
+++ Week 6.R
@@ -0,0 +1,21 @@
+##ADVANCED TECHNIQUES
+#Highly recommended
+  #An Introduction to Statistical Learning with Applications in R
+    #Free online
+
+#Decision trees
+  #Attempts to predict an outcome variable using some set of predictor variables
+  #Approach with recursive binary partitioning
+  #Binary partitioning (what Preston was talking about!)
+    #Does it over an over again
+
+#Based on examples
+  #Within differences and between differences
+    #within small and between big
+
+#An ANOVA but broken down
+
+##Ashley's Presentation: Cluster Analysis
+
+
+  
